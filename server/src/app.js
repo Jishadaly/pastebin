2b6c33f5f3b7da528351bb4845ac717a37756d1e
jishadaly <jishadlm10@gmail.com>
2025-12-30 16:57:43 +0530

    cors added

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const pastesRoutes = require("./routes/pasteRoute");
 const errorHandler = require("./middleware.js/errorHandler");
+const cors = require('cors')
 
 const app = express();
+
+const corsOptions = {
+    origin: "*",
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+};
+
+app.use(cors(corsOptions));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
